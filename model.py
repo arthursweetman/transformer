03ae9ba22a94dcfc42583e5fd0fdcf002a384fa3
Arthur Sweetman <56804555+arthursweetman@users.noreply.github.com>
2023-12-31 11:00:08 -0600

    Added FFN class to transformer

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 d_model=512
+d_ff = 2048
 dropout = 0.2
 
 class Head(nn.Module):
@@ -42,10 +43,34 @@ class MultiHeadAttention(nn.Module):
         scores = self.w_o(scores)
         return scores
 
+class FeedForward(nn.Module):
+
+    def __init__(self, d_model, d_ff=2048):
+        super().__init__()
+        self.d_model = d_model
+        self.layer1 = nn.Linear(d_model, d_ff)
+        self.layer2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x):
+        if x.shape[-1] != self.d_model:
+            raise ValueError(
+                f"Embedding length of input is not equal to d_model: {x.shape[-1]} != {self.d_model}.")
+        l1 = self.layer1(x)
+        out1 = torch.relu(l1)
+        l2 = self.layer2(out1)
+        return l2
+
+
+
 if __name__ == "__main__":
-    x = torch.randn((1, 8, 512))
-    print(x)
+    x = torch.randn((1, 8, d_model))
+    print('input:', x)
     att = MultiHeadAttention(4, 128)
     scores = att(x)
-    print(scores.shape)
+    print('scores:', scores.shape)
     print(scores)
+    ffn = FeedForward(d_model, d_ff)
+    out_ffn = ffn(scores)
+    print('out_ffn', out_ffn.shape)
+    print(out_ffn)
+
