a5a8411f0a12591b85a89382f2b5ce94f791fb41
Arthur Sweetman <56804555+arthursweetman@users.noreply.github.com>
2023-12-31 11:28:33 -0600

    Added Position Encodings class

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import math
 
 d_model=512
 d_ff = 2048
@@ -60,6 +61,19 @@ class FeedForward(nn.Module):
         l2 = self.layer2(out1)
         return l2
 
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_seq_len=512):
+        super(PositionalEncoding, self).__init__()
+        pe = torch.zeros(max_seq_len, d_model)
+        position = torch.arange(0, max_seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        return x + self.pe[:, :x.size(1)].detach()
 
 
 if __name__ == "__main__":
